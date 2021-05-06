fe53ac1b22211ccce51941f0eabec6d598ac9872
noob <noobie37@protonmail.com>
2021-05-06 19:37:52 +0530

    Level 5 authentication using cookies & passport and sessions

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-// const encrypt = require("mongoose-encryption");
-// const md5 = require("md5");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 
 const app = express();
 
@@ -18,21 +18,37 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(session({ //setup and use session package
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize()); //use initialize passport
+app.use(passport.session()); //use passport session to dealing with this sessions
+
+
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+mongoose.set("useCreateIndex", true);
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 // userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']}); //Level 2 authentication
 
-
-
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser()); //serialize creates the cookie and stuff the message user identification
+passport.deserializeUser(User.deserializeUser()); //allows passport to be able to crumble key and discover the message inside who the user is so we can authenticated them.
 
 
 
@@ -48,49 +64,61 @@ app.get('/register', function(req, res) {
   res.render('register');
 });
 
+app.get('/secrets', function(req, res){
+  if(req.isAuthenticated()){
+    res.render("secrets");
+  }
+  else{
+    res.redirect('/login');
+  }
+});
 
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+// --register--
 
 app.post('/register', function(req, res) {
 
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
-    });
-
-    newUser.save(function(err) {  //mongoose encrypt when save
-      if (!err) {
-        res.render("secrets")
-      } else {
-        console.log(err);
-      }
-    });
+  User.register({
+    username: req.body.username
+  }, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
+    }
   });
+
 });
 
 
-
+// --login--
 
 app.post('/login', function(req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
 
-  User.findOne({  //mongoose decrypt
-    email: username
-  }, function(err, foundUser) {
-    if (!err) {
-      if (foundUser) {
-        bcrypt.compare(password, foundUser.password, function(err, result){
-            if(result === true){res.render("secrets");}
-            else{res.render(err);}
-        });
-      }
-    } else {
-      res.send(err);
-    }
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err){
+    if(err){console.log(err);}
+    else{passport.authenticate("local")(req, res, function(){
+      res.redirect('/secrets');
+    })}
   })
 
 });
+
+
+
+
 
 app.listen(3000, function() {
   console.log("server started on port 3000!");
