b1950e37d9dad3c428f563bc46fef851b986eadf
noob <noobie37@protonmail.com>
2021-05-07 12:46:17 +0530

    update Procfile and app.js

--- app.js
+++ app.js
@@ -191,9 +191,12 @@ app.post('/login', function(req, res) {
 });
 
 
+let port = process.env.PORT;
+if(port == null || port == "") {
+  port = 3000;
+}
 
 
-
-app.listen(process.env.PORT || 3000, function() {
+app.listen(port, function() {
   console.log("server started successfully!");
 })
